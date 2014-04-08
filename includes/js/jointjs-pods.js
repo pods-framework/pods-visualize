97c5a2819c0aed02172fc09087120d486849059f
pglewis <pglewis13@gmail.com>
2014-04-08 00:00:20 -0400

    Refactor, pass one

--- includes/js/jointjs-pods.js
+++ includes/js/jointjs-pods.js
@@ -1,23 +1,67 @@
-
+/**
+ *
+ * @type {*|void|extend|extend|extend|extend}
+ */
 joint.shapes.basic.Pod = joint.shapes.basic.Generic.extend( {
 
-	markup : '<g class="rotatable"><g class="scalable"><rect class="pod"/></g><text/></g>',
+	markup : '<g class="rotatable"><g class="scalable"><rect class="pod"/></g><g class="scalable"><rect class="magnet"/></g><text/></g>',
+
+	color_codes : {
+		'post_type' : '#06c0f1',
+		'cpt' : '#06c0f1',
+		'taxonomy' : '#546e7d',
+		'ct' : '#f7a458',
+		'user' : '#5e53a2',
+		'media' : '#9f7d7e',
+		'comment' : '#ef5549',
+		'pod' : '#8dbc6c',
+		'settings' : '#00ada4',
+		'custom-simple' : '#ba81b8'
+	},
+
+	initialize: function() {
+
+		joint.shapes.basic.Generic.prototype.initialize.apply(this, arguments);
+
+		/**
+		 * Pods specific stuff
+		 */
+		this.attr( {
+			'rect.pod': {
+				fill: this.color_codes[ this.get( 'pod_type' ) ]
+			},
+			text: {
+				text: this.get( 'pod_name' )
+			}
+		} );
+
+	},
 
 	defaults : joint.util.deepSupplement( {
 
 		type : 'basic.Pod',
-		size: {
-			width: 250,
-			height: 60
+
+		size : {
+			width : 250,
+			height : 60
 		},
+
 		attrs : {
 			'rect.pod' : {
-				fill : '',
-				'stroke-width': "0",
 				stroke : '',
+				'stroke-width' : "0",
 				width : 1,
 				height : 1
 			},
+
+			'rect.magnet' : {
+				magnet : true,
+				fill : '',
+				stroke : '',
+				width : 1,
+				height : 60
+			},
+
 			'text' : {
 				text : '',
 				'ref-x' : .5,
@@ -25,55 +69,91 @@ joint.shapes.basic.Pod = joint.shapes.basic.Generic.extend( {
 				ref : 'rect.pod',
 				'y-alignment' : 'middle',
 				'x-alignment' : 'middle',
-				fill: '#ffffff',
-				'font-family': 'Times',
-				'font-size': 20,
-				'font-weight': 'normal'
+				fill : '#ffffff',
+				'font-family' : 'Times',
+				'font-size' : 20,
+				'font-weight' : 'normal'
 			}
 		}
 
 	}, joint.shapes.basic.Generic.prototype.defaults )
 } );
 
+/**
+ *
+ * @type {*|void|extend|extend|extend|extend}
+ */
+joint.dia.PodsLink = joint.dia.Link.extend( {
 
-joint.shapes.basic.PodsRelationship = joint.shapes.basic.Generic.extend( {
+	single_fill: '#bdc3c7',
+	multi_fill: '#dd2222',
 
-	markup : '<g class="rotatable"><g class="scalable"><rect class="pod"/></g><g class="scalable"><rect class="magnet"/></g><text/></g>',
+	initialize: function() {
+
+		joint.dia.Link.prototype.initialize.apply(this, arguments);
+
+		// Link attributes
+		this.attr( {
+			'.marker-target': {
+				fill: ( this.get('is_multi') ) ? this.multi_fill : this.single_fill,
+				d: 'M 10 0 L 0 5 L 10 10 z'
+			}
+		} );
+
+		// Bi-directional?
+		if ( this.get('bidirectional').hasOwnProperty( 'sister_field_name' ) ) {
+
+			this.attr( {
+				'.connection': {
+					'stroke-dasharray': ''
+				},
+				'.marker-source': {
+					fill: ( this.get( 'bidirectional' ).is_multi ) ? this.multi_fill : this.single_fill,
+					d: 'M 10 0 L 0 5 L 10 10 z'
+				}
+			} );
+
+			this.label( 1, {
+				position: 50,
+				attrs: {
+					text: {
+						text: this.get( 'bidirectional' ).sister_field_name,
+						'font-family': 'Courier New',
+						'font-size': 11
+					}
+				}
+			} );
+		}
+
+		// Link label
+		this.label( 0, {
+			position: -50,
+			attrs: {
+				text: {
+					text: this.get( 'relationship_label' ),
+					'font-family': 'Courier New',
+					'font-size': 11
+				}
+			}
+		} );
+
+	},
 
 	defaults : joint.util.deepSupplement( {
 
-		type : 'basic.PodsRelationship',
-		size: {
-			width: 250,
-			height: 60
-		},
+		type : 'PodsLink',
+
+		smooth: true,
+
 		attrs : {
-			'rect.pod' : {
-				stroke : '',
-				'stroke-width': "0",
-				width : 1,
-				height : 1
-			},
-			'rect.magnet' : {
-				magnet: true,
-				fill: '',
-				stroke: '',
-				width: 1,
-				height: 60
-			},
-			'text' : {
-				text : '',
-				'ref-x' : .5,
-				'ref-y' : .5,
-				ref : 'rect.pod',
-				'y-alignment' : 'middle',
-				'x-alignment' : 'middle',
-				fill: '#ffffff',
-				'font-family': 'Times',
-				'font-size': 20,
-				'font-weight': 'normal'
+			'.connection': {
+				stroke: '#646d72',
+				'stroke-dasharray': '2 4',
+				'stroke-width': 2
 			}
+
 		}
 
-	}, joint.shapes.basic.Generic.prototype.defaults )
+	}, joint.dia.Link.prototype.defaults )
+
 } );
--- includes/js/pods-visualize.js
+++ includes/js/pods-visualize.js
@@ -1,203 +1,108 @@
-(function ( $ ) {
-	"use strict";
+/**
+ *
+ */
+jQuery( function ( $ ) {
 
-	$(function () {
+	pods_visualize.render(  $( '#paper' ),  pods_visualization_data ); // global, passed via wp_localize_script
 
-		var pod_data = pods_visualization_data; // global, passed via wp_localize_script
+} );
 
-		var single_fill = '#bdc3c7';
-		var multi_fill = '#dd2222';
-		var color_codes = {
-			'post_type': '#06c0f1',
-			'cpt': '#06c0f1',
-			'taxonomy': '#546e7d',
-			'ct': '#f7a458',
-			'user': '#5e53a2',
-			'media': '#9f7d7e',
-			'comment': '#ef5549',
-			'pod': '#8dbc6c',
-			'settings': '#00ada4',
-			'custom-simple': '#ba81b8'
-		};
+pods_visualize = {};
 
-		// ToDo: Magic numbers
-		var y_offset = 85;
-		var x_offset = 600;
+/**
+ *
+ */
+pods_visualize.render = function( element, pod_data ) {
 
-		var graph = new joint.dia.Graph;
+	this.graph = new joint.dia.Graph;
 
-		var paper = new joint.dia.Paper( {
-			el: $( '#paper' ),
-			width: 800,
-			height: 600,
-			gridSize: 1,
-			model: graph
+	this.paper = new joint.dia.Paper( {
+		linkView: joint.dia.LinkView,
+		el: element,
+		width: 800,
+		height: 600,
+		gridSize: 1,
+		model: this.graph
+	} );
+
+	var new_link;
+
+	// ToDo: Magic numbers
+	var y_offset = 85;
+	var x_offset = 600;
+
+	// Iterate the pods
+	var y = 10;
+	var x = 10;
+	for ( var pod_key in pod_data ) {
+
+		if ( !pod_data.hasOwnProperty( pod_key ) ) {
+			continue;
+		}
+
+		var this_pod = pod_data[ pod_key ];
+		var new_y = y;
+
+		var pod_element = new joint.shapes.basic.Pod( {
+			pod_name: this_pod[ 'name' ],
+			pod_type: this_pod[ 'type' ]
 		} );
+		pod_element.translate( x, y );
+		this.graph.addCell( pod_element );
 
+		// Iterate the fields in this pod
+		for ( var relationship_field_name in this_pod[ 'relationships' ] ) {
 
-		// Iterate the pods
-		var y = 10;
-		var x = 10;
-		for ( var pod_key in pod_data ) {
-
-			if ( !pod_data.hasOwnProperty( pod_key ) ) {
+			if ( !this_pod[ 'relationships' ].hasOwnProperty( relationship_field_name ) ) {
 				continue;
 			}
 
-			var this_pod = pod_data[ pod_key ];
-			var new_y = y;
+			var this_relationship = this_pod[ 'relationships' ][ relationship_field_name ];
 
-			var pod_element = element( x, y, this_pod[ 'name' ], color_codes[ this_pod[ 'type' ] ], false );
-
-			// Iterate the fields in this pod
-			for ( var relationship_field_name in this_pod[ 'relationships' ] ) {
-
-				if ( !this_pod[ 'relationships' ].hasOwnProperty( relationship_field_name ) ) {
-					continue;
-				}
-
-				var this_relationship = this_pod[ 'relationships' ][ relationship_field_name ];
-
-				var related_element = element( x + x_offset, new_y, this_relationship[ 'related_pod_name' ], color_codes[ this_relationship[ 'type' ] ], true );
-				link ( pod_element, related_element, relationship_field_name, this_relationship[ 'is_multi' ], this_relationship[ 'bidirectional' ] );
-
-				new_y += y_offset;
-			}
-
-			var parent_y = new_y - y;
-
-			// Any relationships?
-			if ( 0 != parent_y ) {
-
-				// Subtract out the final increment and center vertically
-				parent_y -= y_offset;
-				parent_y /= 2;
-			}
-			else {
-				// No relationships pushed the y offset, so add it
-				new_y += y_offset;
-			}
-
-			// Re-position the parent pod
-			pod_element.translate( x, parent_y );
-
-			y = new_y;
-		}
-
-		paper.fitToContent( 1, 1, 100 );
-
-		/**
-		 *
-		 * @param x
-		 * @param y
-		 * @param label
-		 * @param fill
-		 * @param related
-		 * @returns {joint.shapes.basic.Pod} | {joint.shapes.basic.PodsRelationship}
-		 */
-		function element ( x, y, label, fill, related ) {
-
-			var new_element;
-			if ( related ) {
-				new_element = new joint.shapes.basic.PodsRelationship( {
-					position: { x: x,  y: y }
-				} );
-			}
-			else {
-				new_element = new joint.shapes.basic.Pod( {
-					position: { x: x,  y: y }
-				} );
-			}
-
-			new_element.attr( {
-				'rect.pod': {
-					fill: fill
-				},
-				text: {
-					text: label
-				}
+			var related_element = new joint.shapes.basic.Pod( {
+				pod_name: this_relationship[ 'related_pod_name' ],
+				pod_type: this_relationship[ 'type' ]
 			} );
+			//var related_element = this.element( this_relationship[ 'related_pod_name' ], this_relationship[ 'type' ] );
+			related_element.translate( x + x_offset, new_y );
+			this.graph.addCell( related_element );
 
-			graph.addCell( new_element );
-			return new_element;
-		}
 
-		/**
-		 * @param elm1
-		 * @param elm2
-		 * @param label
-		 * @param is_multi
-		 * @param bidirectional
-		 * @returns {joint.dia.Link}
-		 */
-		function link ( elm1, elm2, label, is_multi, bidirectional ) {
-
-			var new_link = new joint.dia.Link( {
-				source: { id: elm1.id },
+			new_link = new joint.dia.PodsLink( {
+				relationship_label: relationship_field_name,
+				is_multi: this_relationship[ 'is_multi' ],
+				bidirectional: this_relationship[ 'bidirectional' ],
+				source: { id: pod_element.id },
 				target: {
-					id: elm2.id,
+					id: related_element.id,
 					selector: '.magnet'
 				}
 			} );
 
-			// Link attributes
-			new_link.attr( {
-				'.connection': {
-					stroke: '#646d72',
-					'stroke-dasharray': '2 4',
-					'stroke-width': 2
-				},
-				'.marker-target': {
-					fill: ( is_multi ) ? multi_fill : single_fill,
-					d: 'M 10 0 L 0 5 L 10 10 z'
-				}
-			} );
+			this.graph.addCell( new_link );
 
-			// Bi-directional?
-			if ( bidirectional.hasOwnProperty( 'sister_field_name' ) ) {
-
-				new_link.attr( {
-					'.connection': {
-						'stroke-dasharray': ''
-					},
-					'.marker-source': {
-						fill: ( bidirectional[ 'is_multi' ] ) ? multi_fill : single_fill,
-						d: 'M 10 0 L 0 5 L 10 10 z'
-					}
-				} );
-
-				new_link.label( 1, {
-					position: 50,
-					attrs: {
-						text: {
-							text: bidirectional[ 'sister_field_name' ],
-							'font-family': 'Courier New',
-							'font-size': 11
-						}
-					}
-				} );
-			}
-
-			// Link presentation
-			new_link.set( 'smooth', true );
-
-			// Link label
-			new_link.label( 0, {
-				position: -50,
-				attrs: {
-					text: {
-						text: label,
-						'font-family': 'Courier New',
-						'font-size': 11
-					}
-				}
-			} );
-
-
-			graph.addCell( new_link );
-			return new_link;
+			new_y += y_offset;
 		}
 
-	});
+		var parent_y = new_y - y;
 
-}(jQuery));
+		// Any relationships?
+		if ( 0 != parent_y ) {
+
+			// Subtract out the final increment and center vertically
+			parent_y -= y_offset;
+			parent_y /= 2;
+		}
+		else {
+			// No relationships pushed the y offset, so add it
+			new_y += y_offset;
+		}
+
+		// Re-position the parent pod
+		pod_element.translate( x, parent_y );
+
+		y = new_y;
+	}
+
+	this.paper.fitToContent( 1, 1, 100 );
+};
